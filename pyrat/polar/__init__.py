70221664991359c7a85bd855c19bfa136b969cee
Maxim Neumann <maxim.neumann@jpl.nasa.gov>
2015-04-01 22:02:55 -0700

    Minor bugfixes to get the info system working

--- pyrat/polar/__init__.py
+++ pyrat/polar/__init__.py
@@ -14,13 +14,12 @@ def info():
     from inspect import getmembers, isclass
     current_module = sys.modules[__name__]
     modules = getmembers(current_module, isclass)
-    logging.info()
+    logging.info("-")
     logging.info("Content of module "+__name__+":")
-    logging.info()
+    logging.info("-")
     for mod in modules:
         if 'pyrat' in mod[1].__module__:
             doc = str(mod[1].__doc__)
             if doc != 'None':
                 doc = doc.split('\n')[1]
             logging.info(mod[0].ljust(20)+doc)
-
